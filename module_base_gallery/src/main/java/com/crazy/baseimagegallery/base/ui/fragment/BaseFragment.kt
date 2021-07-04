8d5483c4339d3cdf2f05735067bec80bb350930f
zhouyu <2210518393@qq.com>
2021-07-05 01:01:09 +0800

    新增Basefragment，需要写个test类；

---
+++ module_base_gallery/src/main/java/com/crazy/baseimagegallery/base/ui/fragment/BaseFragment.kt
@@ -0,0 +1,64 @@
+package com.crazy.baseimagegallery.base.ui.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.crazy.baseimagegallery.R
+import com.crazy.baseimagegallery.util.StateView
+
+abstract class BaseFragment :Fragment() {
+
+    /** 多状态布局View*/
+    private var statusView: StateView? = null
+    private lateinit var  rootView: View
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+         rootView = inflater.inflate(getLayoutId(),null)
+        return rootView
+//        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+     abstract fun getLayoutId():Int
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        if (isSetStateView()) {
+            setStatusLayout()
+        }
+
+        initView ()
+        initData ()
+
+
+    }
+
+    /** 显示Loading*/
+    fun showLoading() {
+        statusView?.showLoading()
+    }
+
+    /** 隱藏Loading*/
+    fun dismissLoading() {
+        statusView?.showContent()
+    }
+
+    open fun isSetStateView(): Boolean = true
+
+    private fun setStatusLayout() {
+        val contentView: ViewGroup? = rootView.findViewById(R.id.content_id)
+        if (contentView != null) {
+            statusView = StateView.inject(contentView)
+        }
+    }
+
+    abstract fun initView()
+    abstract fun initData()
+}
