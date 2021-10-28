40ec5b3d5cb8decb46dac8d4f3bab2bbda099d2c
zhouyu <2210518393@qq.com>
2021-10-28 17:55:08 +0800

    适配Android Q 新增暗黑模式

--- app/src/main/java/com/crazy/commonapplication/MainActivity.kt
+++ app/src/main/java/com/crazy/commonapplication/MainActivity.kt
@@ -1,18 +1,26 @@
 package com.crazy.commonapplication
 
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatDelegate
 import com.crazy.baseimagegallery.base.ui.activity.BaseActivity
 import com.crazy.commonapplication.databinding.ActivityMainBinding
 
 class MainActivity : BaseActivity<ActivityMainBinding>() {
 
-
+    private var isNight =false
     override fun getViewBinding(): ActivityMainBinding {
         return ActivityMainBinding.inflate(layoutInflater)
     }
 
     override fun initView() {
-        viewBing.tv.text = "测试测试测试测试测试测试测试"
+//        viewBing.tv.text = "测试测试测试测试测试测试测试"
+//        viewBing.tv.setTextColor(resources.getColor(R.color.red))
 
+        viewBing.bt.setOnClickListener {
+
+            AppCompatDelegate.setDefaultNightMode( AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
+        }
     }
 
     override fun initData() {
